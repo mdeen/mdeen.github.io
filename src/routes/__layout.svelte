b6514d4489502817aa6ecf9c18de82c0e1cf3822
Maya Deen <m2deen@outlook.com>
2021-10-01 09:02:37 -0700

    Added header, subheadings and Yosemite background

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,68 @@
+<script>
+    import "../styles.css"
+</script>
+
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+</svelte:head>
+
+<header class='page-header'>
+    <h1 class="website-title">maya.cosmos</h1>
+    <nav class="main-nav" aria-label="Main menu">
+        <ul class="main-menu">
+            <a href='#' class='main-menu-entry'>Portfolio</a>
+            <a href='#' class='main-menu-entry'>Notes</a>
+            <a href='#' class='main-menu-entry'>About</a>
+        </ul>
+    </nav>
+</header>
+
+<slot>
+
+</slot>
+
+<footer>
+
+</footer>
+
+<style>
+    :global(body) {
+        background-image: url("/yosemite-sunset.jpg");
+        background-size: cover;
+        background-attachment: fixed;
+        background-position: center;
+    }
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        width: 100vw;
+        font-family: 'Roboto', sans-serif;
+        color: hsl(var(--color-primary-000));
+    }
+    h1.website-title {
+        padding: var(--px-height-XS);
+        font-weight: var(--font-weight-100);
+        font-size: var(--px-height-XXL);
+    }
+    nav {
+        align-self: center;
+    }
+    ul.main-menu {
+        display: flex;
+        flex-grow: 1;
+        align-items: center;
+        font-weight: var(--font-weight-300);
+        font-size: var(--px-height-M);
+    }
+    a {
+        text-decoration: none;
+        color:#474950;
+        padding: var(--px-height-XS);
+    }
+    a:visited {
+        color: hsl(var(--color-primary-000));
+    }
+</style>
---
+++ src/routes/test-color-palette.svelte
@@ -0,0 +1,145 @@
+<!-- <div class='warm-grey palette'>
+    <h1 class='warm-grey000'>Maya.Cosmos</h1>
+    <h1 class='warm-grey100'>Maya.Cosmos</h1>
+    <h1 class='warm-grey200'>Maya.Cosmos</h1>
+    <h1 class='warm-grey300'>Maya.Cosmos</h1>
+    <h1 class='warm-grey400'>Maya.Cosmos</h1>
+    <h1 class='warm-grey500'>Maya.Cosmos</h1>
+    <h1 class='warm-grey600'>Maya.Cosmos</h1>
+    <h1 class='warm-grey700'>Maya.Cosmos</h1>
+    <h1 class='warm-grey800'>Maya.Cosmos</h1>
+    <h1 class='warm-grey900'>Maya.Cosmos</h1> 
+</div> -->
+<!-- <div class="primary palette">
+    <h1 class='primary000'>Maya.Cosmos</h1>
+    <h1 class='primary100'>Maya.Cosmos</h1>
+    <h1 class='primary200'>Maya.Cosmos</h1>
+    <h1 class='primary300'>Maya.Cosmos</h1>
+    <h1 class='primary400'>Maya.Cosmos</h1>
+    <h1 class='primary500'>Maya.Cosmos</h1>
+    <h1 class='primary600'>Maya.Cosmos</h1>
+    <h1 class='primary700'>Maya.Cosmos</h1>
+    <h1 class='primary800'>Maya.Cosmos</h1>
+    <h1 class='primary900'>Maya.Cosmos</h1>
+</div>
+<div class="secondary palette">
+    <h1 class='secondary000'>Maya.Cosmos</h1>
+    <h1 class='secondary100'>Maya.Cosmos</h1>
+    <h1 class='secondary200'>Maya.Cosmos</h1>
+    <h1 class='secondary300'>Maya.Cosmos</h1>
+    <h1 class='secondary400'>Maya.Cosmos</h1>
+    <h1 class='secondary500'>Maya.Cosmos</h1>
+    <h1 class='secondary600'>Maya.Cosmos</h1>
+    <h1 class='secondary700'>Maya.Cosmos</h1>
+    <h1 class='secondary800'>Maya.Cosmos</h1>
+    <h1 class='secondary900'>Maya.Cosmos</h1>
+</div> -->
+
+<style>
+    /* h1.warm-grey000 {
+        color: hsl(var(--color-warm-grey-000));
+    }
+    h1.warm-grey100 {
+        color: hsl(var(--color-warm-grey-100));
+    }
+    h1.warm-grey200 {
+        color: hsl(var(--color-warm-grey-200));
+    }
+    h1.warm-grey300 {
+        color: hsl(var(--color-warm-grey-300));
+    }
+    h1.warm-grey400 {
+        color: hsl(var(--color-warm-grey-400));
+    }
+    h1.warm-grey500 {
+        color: hsl(var(--color-warm-grey-500));
+    }
+    h1.warm-grey600 {
+        color: hsl(var(--color-warm-grey-600));
+    }
+    h1.warm-grey700 {
+        color: hsl(var(--color-warm-grey-700));
+    }
+    h1.warm-grey800 {
+        color: hsl(var(--color-warm-grey-800));
+    }
+    h1.warm-grey900 {
+        color: hsl(var(--color-warm-grey-900));
+    }
+
+    .primary.palette {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        width: 100vw;
+        font-family: 'Roboto', sans-serif;
+    }
+    h1.primary000 {
+        color: hsl(var(--color-primary-000));
+    }
+    h1.primary100 {
+        color: hsl(var(--color-primary-100));
+    }
+    h1.primary200 {
+        color: hsl(var(--color-primary-200));
+    }
+    h1.primary300 {
+        color: hsl(var(--color-primary-300));
+    }
+    h1.primary400 {
+        color: hsl(var(--color-primary-400));
+    }
+    h1.primary500 {
+        color: hsl(var(--color-primary-500));
+    }
+    h1.primary600 {
+        color: hsl(var(--color-primary-600));
+    }
+    h1.primary700 {
+        color: hsl(var(--color-primary-700));
+    }
+    h1.primary800 {
+        color: hsl(var(--color-primary-800));
+    }
+    h1.primary900 {
+        color: hsl(var(--color-primary-900));
+    }
+    
+    .secondary.palette {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        width: 100vw;
+        font-family: 'Roboto', sans-serif;
+    }
+    h1.secondary000 {
+        color: hsl(var(--color-secondary-000));
+    }
+    h1.secondary100 {
+        color: hsl(var(--color-secondary-100));
+    }
+    h1.secondary200 {
+        color: hsl(var(--color-secondary-200));
+    }
+    h1.secondary300 {
+        color: hsl(var(--color-secondary-300));
+    }
+    h1.secondary400 {
+        color: hsl(var(--color-secondary-400));
+    }
+    h1.secondary500 {
+        color: hsl(var(--color-secondary-500));
+    }
+    h1.secondary600 {
+        color: hsl(var(--color-secondary-600));
+    }
+    h1.secondary700 {
+        color: hsl(var(--color-secondary-700));
+    }
+    h1.secondary800 {
+        color: hsl(var(--color-secondary-800));
+    }
+    h1.secondary900 {
+        color: hsl(var(--color-secondary-900));
+    }  */
+</style>
